b01969ce0fdcdae51095cb741d0f864702db81a9
burnpiro <kemalpiro@gmail.com>
2020-05-21 09:44:19 +0200

    Updates

---
+++ precache-manifest.06b0a72ca1d5a22f81d20cbe98be4f59.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5db966d92b6a9831a281fe7017e31689",
+    "url": "/wod-bike-dataset-generator/index.html"
+  },
+  {
+    "revision": "57368a20c569498845fb",
+    "url": "/wod-bike-dataset-generator/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "249d21a56a44ce176df5",
+    "url": "/wod-bike-dataset-generator/static/js/2.8d6b76a6.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/wod-bike-dataset-generator/static/js/2.8d6b76a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57368a20c569498845fb",
+    "url": "/wod-bike-dataset-generator/static/js/main.0dbedb90.chunk.js"
+  },
+  {
+    "revision": "c3ac19e07d3aeda0f948",
+    "url": "/wod-bike-dataset-generator/static/js/runtime-main.ecbcacda.js"
+  }
+]);
