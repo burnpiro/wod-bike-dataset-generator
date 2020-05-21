40ddb69785682c76ea48a9932e68e516221230c0
burnpiro <kemalpiro@gmail.com>
2020-05-21 09:45:48 +0200

    Arrange components

--- application/src/components/App/App.js
+++ application/src/components/App/App.js
@@ -1,12 +1,10 @@
 import React from "react";
-import logo from "./logo.svg";
-import Layout from "./Layout";
+import Layout from "../Layout/Layout";
 import "./App.css";
 
 function App() {
   return (
     <Layout>
-      <img src={logo} className="App-logo" alt="logo" />
     </Layout>
   );
 }
