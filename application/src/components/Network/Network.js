7de847cb85dab9c299efbf6cc434c5914e1b672f
burnpiro <kemalpiro@gmail.com>
2020-05-21 16:09:09 +0200

    Load bike stations and display them on the map

--- application/src/components/Network/Network.js
+++ application/src/components/Network/Network.js
@@ -1,11 +1,11 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import Plot from "react-plotly.js";
+
+import nodes from './nodes_locations.csv'
 
 const data = [
   {
-    type: "scattergeo",
-    mode: "markers",
-    locations: ["FRA", "DEU", "RUS", "ESP"],
+    type: "scattermapbox",
     marker: {
       size: [20, 30, 15, 10],
       color: [10, 20, 40, 50],
@@ -21,33 +21,52 @@ const data = [
         color: "black",
       },
     },
-    name: "europe data",
   },
 ];
 
 const layout = {
-  geo: {
-    scope: "europe",
-    resolution: 50,
+  dragmode: "zoom",
+  mapbox: {
+    style: "open-street-map",
+    center: { lat: 51.11, lon: 17.03 },
+    margin: { r: 0, t: 0, b: 0, l: 0 },
+    zoom: 11,
   },
-  autosize: true
+  autosize: true,
+  showlegend: false
 };
 
-const config = {
-  responsive: true
-};
+const Network = () => {
+  const [fetchedCSVData, setFetchedCSVdata] = useState(null);
 
-class Network extends React.Component {
-  render() {
-    return (
-      <Plot
-        data={data}
-        layout={layout}
-        useResizeHandler={true}
-        style={{width:"100%", minHeight: "calc(100vh - 200px)"}}
-      />
-    );
-  }
+  useEffect(() => {
+    if (!fetchedCSVData) {
+      fetch(nodes)
+        .then(res => res.text())
+        .then(stringData => {
+          let lines = stringData.split("\n").map(line => line.split(','));
+          let [ idx, ...data] = lines
+          setFetchedCSVdata(data.map(point => ({
+            type:'scattermapbox',
+            lat:[point[2]],
+            lon:[point[3]],
+            mode:'markers',
+            marker: {
+              size:14
+            },
+            text:[point[1]]
+          })));
+        });
+    }
+  }, [])
+  return (
+    <Plot
+      data={fetchedCSVData != null ? fetchedCSVData : data}
+      layout={layout}
+      useResizeHandler={true}
+      style={{ width: "100%", minHeight: "calc(100vh - 70px)" }}
+    />
+  );
 }
 
 export default Network;
