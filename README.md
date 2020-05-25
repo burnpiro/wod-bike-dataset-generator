f21ff21e165f6494d1dc12ef0b24943e9ea01a4d
Kemal Erdem <kemalpiro@gmail.com>
2020-05-25 20:58:27 +0200

    Revert "Task/create edges paths" (#16)
    * Revert "Done generating paths, Add fake file generator for web page,"
    
    This reverts commit c61bf0b5e1afa24fc0ee3c7f5e2ab53c38f285c7.
    
    * Revert "Move old notebooks"
    
    This reverts commit 570b2babdd02321a46d0c894605d78f0580e2fb3.
    
    * Revert "Move old notebooks"
    
    This reverts commit 61712399cfd1963b8279e4ef56577a728d9b4664.
    
    * Revert "Generate paths"
    
    This reverts commit 13c49e80eb18b3996541b48552d89020df04f7a4.
    
    * Revert "Create network with slider"
    
    This reverts commit 9b35709e2207853aada6716e340beaf0a5a5c13a.
    
    * Revert "Fetching data hooks"
    
    This reverts commit 0ee04301565376633294c69559ad471845c85e31.
    
    * Revert "Try to add large storage"
    
    This reverts commit c7a2f8f4494e46a6f53fd8fc178f86441e056283.
    
    * Revert "Generate new location"
    
    This reverts commit 47811dbc28175ac57cc74a382720a16dabcae72d.
    
    * Revert "Fix perf issue with multiple layers created"
    
    This reverts commit 5222f9550e060352d5aaa95e7d05897102972173.

--- README.md
+++ README.md
@@ -1,32 +1,5 @@
 # wod-bike-dataset-generator
 
-## Github Large File Storage
-
-For large file storeage we need to use [Github LFS](https://git-lfs.github.com/). Please follow the instruction and install it on your machine.
-
-## Path generation
-To generate csv file with paths you need to have `./network/nodes_locations.csv` file already available.
-
-Execute notebook (watch out, it's memory consuming, and takes ages ~7h):
-```
-OSMNX - path generation.ipynb
-```
-
-File with paths should be separate files depends on accuracy:
- ```markdown
-./application/app_data/paths_reduced-e7.json
-./application/app_data/paths_reduced-e8.json
-./application/app_data/paths_reduced-e9.json
-./application/app_data/paths_reduced-e10.json
-./application/app_data/paths_reduced-e11.json
-./application/app_data/paths_reduced-e12.json
-```
-
-Additionally, it will create file with nodes:
-```markdown
-./application/public/nodes_list.json
-```
-
 ## WebApp installation
 
 ```bash
@@ -41,6 +14,30 @@ npm run deploy
 ```
 
 ## Mapped network nodes on Wroclaw map
-![alt text](./assets/2019-03-with-degrees.png)
+![alt text](./assets/wrm_map.png)
 
-more visualizations in `./assets/`
+## Map for 03 2019, scaled base on edge weight and node degree
+![alt text](./assets/2019-03-with-degrees.png)
+### only display edges with weight > 20
+![alt text](./assets/2019-03-filtered.png)
+
+
+### Animated network 2019
+![Full network animation](./assets/full_map.gif)
+### Networks month/month 2019
+[Full networks](./assets/2019-full-images.zip)
+
+### Animated network 2019 (degree only)
+![Full network animation](./assets/degree_map.gif)
+
+### Degree distribution change
+![alt text](./assets/degree.gif)
+
+### Clustering Coefficent distribution change
+![alt text](./assets/cc.gif)
+
+### Closeness distribution change
+![alt text](./assets/closeness.gif)
+
+### Betweenness distribution change
+![alt text](./assets/betw.gif)
--- application/src/components/Network/Network.js
+++ application/src/components/Network/Network.js
@@ -1,28 +1,28 @@
-import React, { useEffect, useState } from "react";
-import CircularProgress from "@material-ui/core/CircularProgress";
+import React, {useEffect, useState} from "react";
 import Plot from "react-plotly.js";
-import spacetime from "spacetime";
-import useFetchNodes from "../../hooks/useFetchNodes";
-import useTimer from "../../hooks/useTimer";
-import { makeStyles } from "@material-ui/core/styles";
-import useFetchData from "../../hooks/useFetchData";
-import {generateRandomFile, fillPathsWithData} from "../../helpers";
 
-const useStyles = makeStyles((theme) => ({
-  overlay: {
-    position: "fixed",
-    top: 0,
-    left: 0,
-    width: "100vw",
-    height: "100vw",
-    paddingTop: "25%",
-    backgroundColor: "rgba(0,0,0,0.5)",
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-    color: "white",
+import nodes from './nodes_locations.csv'
+
+const data = [
+  {
+    type: "scattermapbox",
+    marker: {
+      size: [20, 30, 15, 10],
+      color: [10, 20, 40, 50],
+      cmin: 0,
+      cmax: 50,
+      colorscale: "Greens",
+      colorbar: {
+        title: "Some rate",
+        ticksuffix: "%",
+        showticksuffix: "last",
+      },
+      line: {
+        color: "black",
+      },
+    },
   },
-}));
+];
 
 const layout = {
   dragmode: "zoom",
@@ -33,240 +33,43 @@ const layout = {
     zoom: 11,
   },
   autosize: true,
-  showlegend: false,
-  updatemenus: [
-    {
-      buttons: [
-        {
-          method: "update",
-          args: ["play"],
-          label: "Play",
-        },
-        {
-          method: "update",
-          args: ["pause"],
-          label: "Pause",
-        },
-      ],
-      name: "action",
-      direction: "left",
-      pad: { t: 10 },
-      showactive: true,
-      type: "buttons",
-      x: 0.0,
-      xanchor: "left",
-      y: 1.1,
-      yanchor: "top",
-    },
-    {
-      buttons: [
-        {
-          args: [3, 31],
-          label: "March",
-          method: "update",
-        },
-        {
-          args: [4, 30],
-          label: "April",
-          method: "update",
-        },
-        {
-          args: [5, 31],
-          label: "May",
-          method: "update",
-        },
-        {
-          args: [6, 30],
-          label: "June",
-          method: "update",
-        },
-        {
-          args: [7, 31],
-          label: "July",
-          method: "update",
-        },
-        {
-          args: [8, 31],
-          label: "August",
-          method: "update",
-        },
-        {
-          args: [9, 30],
-          label: "September",
-          method: "update",
-        },
-        {
-          args: [10, 31],
-          label: "October",
-          method: "update",
-        },
-        {
-          args: [11, 30],
-          label: "November",
-          method: "update",
-        },
-        {
-          args: [12, 31],
-          label: "December",
-          method: "update",
-        },
-      ],
-      name: "month",
-      direction: "down",
-      pad: { t: 10 },
-      showactive: true,
-      type: "dropdown",
-      x: 0.0,
-      xanchor: "left",
-      y: 1.15,
-      yanchor: "top",
-      active: 0,
-      font: { color: "#5072a8" },
-    },
-  ],
-  sliders: [
-    {
-      pad: { t: 5, b: 10 },
-      x: 0.15,
-      y: 1.11,
-      len: 0.85,
-      currentvalue: {
-        visible: true,
-        xanchor: "right",
-        prefix: "Hour: ",
-        font: {
-          color: "#888",
-          size: 20,
-        },
-      },
-      active: 0,
-      name: "Hour",
-      steps: Array(1440 / 15)
-        .fill(0)
-        .map((val, id) => ({
-          label: `${Math.floor((id * 15) / 60)}:${
-            (id * 15) % 60 === 0 ? "00" : (id * 15) % 60
-          }`,
-          args: [id],
-          method: "update",
-        })),
-    },
-  ],
+  showlegend: false
 };
 
 const Network = () => {
-  const classes = useStyles();
-  const [frames, setFrames] = useState([]);
-  const [frameId, setFrameId] = useState(0);
-  const [day, setDay] = useState(1);
-  const [month, setMonth] = useState({ num: 7, days: 31 });
-  const [{ nodes, paths }, isLoading, isError] = useFetchNodes();
-  const [
-    { data: monthData, isLoading: isMonthLoading },
-    doFetchMonth,
-  ] = useFetchData("07.json", []);
-  const randomFile = generateRandomFile();
-  const { time, start, pause, reset, isRunning } = useTimer({
-    endTime: 1440 / 15,
-    initialTime: frameId,
-  });
-  layout.sliders[0].active = time;
-  layout.sliders[1] = {
-    pad: { t: 5, b: 10 },
-    x: 0.15,
-    y: 1.22,
-    len: 0.85,
-    currentvalue: {
-      visible: true,
-      xanchor: "right",
-      prefix: "Day: ",
-      font: {
-        color: "#888",
-        size: 12,
-      },
-    },
-    active: day - 1,
-    name: "Day",
-    steps: Array(month.days)
-      .fill(0)
-      .map((val, id) => ({
-        label: id + 1,
-        args: [id + 1],
-        method: "update",
-      })),
-  };
+  const [fetchedCSVData, setFetchedCSVdata] = useState(null);
 
   useEffect(() => {
-    if(monthData.length !== 0) {
-      setFrames(
-        Array(1440 / 15)
-          .fill(0)
-          .map((val, id) => [...nodes.list, ...randomFile[day][id*15].map(fillPathsWithData(paths.obj))])
-      );
-
-    } else {
-      setFrames(
-        Array(1440 / 15)
-          .fill(0)
-          .map((val, id) => [...nodes.list])
-      );
+    if (!fetchedCSVData) {
+      fetch(nodes)
+        .then(res => res.text())
+        .then(stringData => {
+          let lines = stringData.split("\n").map(line => line.split(','));
+          let [ idx, ...data] = lines
+          setFetchedCSVdata([{
+            type:'scattermapbox',
+            lat:data.map(point => point[2]),
+            lon:data.map(point => point[3]),
+            ids:data.map(point => point[0]),
+            mode:'markers',
+            marker: {
+              size:14,
+              color: 'blue'
+            },
+            hoverinfo: 'text',
+            text:data.map(point => point[1])
+          }]);
+        });
     }
-  }, [isLoading, isMonthLoading, day]);
-  const data = frames[time] || [];
-  const buttonClick = ({menu: {name}, button: {args}}) => {
-    if(name === 'month') {
-      setFrameId(0);
-      reset(0);
-      setMonth({num: args[0], days: args[1]})
-      setDay(1)
-    } else {
-      switch (args[0]) {
-        case "play":
-          start();
-          break;
-        case "pause":
-          pause();
-          break;
-        default:
-          console.log(args[0]);
-          break;
-      }
-    }
-  };
-  const sliderChange = ({ slider: { name }, step: { args } }) => {
-    if (name === "Hour") {
-      setFrameId(args[0]);
-      reset(args[0]);
-    }
-    if (name === "Day") {
-      setDay(args[0]);
-      setFrameId(0);
-      reset(0);
-    }
-  };
+  }, [])
   return (
-    <React.Fragment>
-      <Plot
-        data={data}
-        layout={layout}
-        frames={Array(frames.length)
-          .fill(0)
-          .map((i, id) => ({
-            name: id,
-          }))}
-        onButtonClicked={buttonClick}
-        onSliderChange={sliderChange}
-        useResizeHandler={true}
-        style={{ width: "100%", minHeight: "calc(100vh - 70px)" }}
-      />
-      {(isLoading) && (
-        <div className={classes.overlay}>
-          <CircularProgress color="secondary" size={80} thickness={4} />
-          <h1>Network data is loading... Please wait :)</h1>
-        </div>
-      )}
-    </React.Fragment>
+    <Plot
+      data={fetchedCSVData != null ? fetchedCSVData : data}
+      layout={layout}
+      useResizeHandler={true}
+      style={{ width: "100%", minHeight: "calc(100vh - 70px)" }}
+    />
   );
-};
+}
 
 export default Network;
